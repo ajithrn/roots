413ff18ef226b1ae864c92a19d0a74d99271cdd2
Ajith R Nair <trytoinnovate@gmail.com>
2014-10-26 12:25:01 +0530

    Update entry-meta.php

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,6 +1,6 @@
 <div class="meta">
 	<time class="published" datetime="<?php echo get_the_time('c'); ?>"> <?php echo __('<span>Posted on :</span>', 'roots'); ?>	<?php echo get_the_date(); ?></time>
 	<div class="byline author vcard"><?php echo __('<span>By :</span>', 'roots'); ?>
-		<a href="<?php // echo get_author_posts_url(get_the_author_meta('ID')); ?>#" rel="author" class="fn"><?php echo get_the_author(); ?></a>
+		<a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>#" rel="author" class="fn"><?php echo get_the_author(); ?></a>
 	</div>
 </div>
