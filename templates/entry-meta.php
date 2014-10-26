c47564ea372814d25e7a920b87ee16c1ff0e03af
Ajith R Nair <trytoinnovate@gmail.com>
2014-10-26 12:24:37 +0530

    Update entry-meta.php

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,2 +1,6 @@
-<time class="updated" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
-<p class="byline author vcard"><?php echo __('By', 'roots'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></p>
+<div class="meta">
+	<time class="published" datetime="<?php echo get_the_time('c'); ?>"> <?php echo __('<span>Posted on :</span>', 'roots'); ?>	<?php echo get_the_date(); ?></time>
+	<div class="byline author vcard"><?php echo __('<span>By :</span>', 'roots'); ?>
+		<a href="<?php // echo get_author_posts_url(get_the_author_meta('ID')); ?>#" rel="author" class="fn"><?php echo get_the_author(); ?></a>
+	</div>
+</div>
